ba5615e7ea13d2151e1c608a99eaa98e59e4ee1a
aathilaksh <lakshmiu@torus.tech>
2024-01-31 19:18:32 +0530

    add nextauth file

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,27 @@
-import Image from "next/image";
+// import Image from "next/image";
+"use client"
+import UserForm from "./component/Userform";
+import { signIn, useSession } from "next-auth/react";
+
 
 export default function Home() {
+
+  const { data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      alert("please signin to continue");
+      signIn();
+    },
+  });
+  console.log(session);
+
+
   return (
-  <>welcome</>
-  );
+
+
+    <UserForm />
+  )
+
+
+
 }
